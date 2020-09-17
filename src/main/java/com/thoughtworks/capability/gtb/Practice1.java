07055a61458e13320f0a3dd4fb81d2ded0c03253
MSI-PV <805560812@qq.com>
2020-09-17 19:36:04 +0800

    完成practice1

--- src/main/java/com/thoughtworks/capability/gtb/Practice1.java
+++ src/main/java/com/thoughtworks/capability/gtb/Practice1.java
@@ -1,6 +1,9 @@
 package com.thoughtworks.capability.gtb;
 
+import com.sun.org.apache.regexp.internal.RE;
+
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 /**
  * 计算任意日期与下一个劳动节相差多少天
@@ -11,6 +14,12 @@ import java.time.LocalDate;
 public class Practice1 {
 
   public static long getDaysBetweenNextLaborDay(LocalDate date) {
-    return 0;
+    int year = LocalDate.now().getYear();
+    LocalDate laborDayOfTheTear = LocalDate.of(year,5,1);
+    if (date.isBefore(laborDayOfTheTear)){
+      return date.until(laborDayOfTheTear, ChronoUnit.DAYS);
+    }
+    LocalDate laborDayOfTheNextYear = laborDayOfTheTear.plusYears(1);
+    return date.until(laborDayOfTheNextYear,ChronoUnit.DAYS);
   }
 }
